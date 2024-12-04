13f96ea37ba63f2fba92e37e362dc18c91d032d5
jainam18 <jainam.rajput18@gmail.com>
2024-12-03 21:06:36 -0600

    Defined the basic functions

---
+++ basic_functions.py
@@ -0,0 +1,17 @@
+def initialize_graph(city_name):
+    """This function when given a city name in format 'City, State, Country',
+    will return a city graph with all the location coordinates as node so in return we get node points and edge as two datasets.
+    """
+    pass
+
+def find_hub_node(G):
+    """Find the central hub node which will be the delivery hub decided according to the edge centrality measure"""
+    pass
+
+def generate_delivery_points(G, no_of_orders):
+    """It generates a n number of delivery points from a graph G of a city where deliveries are to be made."""
+    pass
+
+def route_optimization(hub,delivery_location):
+    """Given the hub and the delivery locations which is a list compute the route with the vehicle every time moving to the shortest distance location"""
+
